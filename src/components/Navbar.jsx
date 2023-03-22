49bc1eb99f8af897986b72740310007b678b41f8
Farhan Khan <farhan.k2005@gmail.com>
2023-03-22 10:16:25 -0400

    changed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 scale-105 object-contain" />
+          <img src='/src/assets/logo2.svg' alt="logo" className="w-9 h-9 scale-105 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Farhan Khan &nbsp;
             <span className="sm:block hidden">|&nbsp; Lead Software Engineer</span>
