c78fd4749ecbe836cbe264253946112650f72540
Farhan Khan <farhan.k2005@gmail.com>
2023-05-04 19:28:39 -0400

    stoped animation from continourly happening

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -9,7 +9,7 @@ const SectionWrapper = (Component, idName) => {
       variants={staggerContainer()}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
+      viewport={{ once: true, amount: 0.25 }}
       className={`${styles.padding} max-w-7x1 mx-auto relative z-o`}
     >
       <span className="hash-span" id={idName}></span>
